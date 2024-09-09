877532c5fbb429540d52476feaff5dc21d9b8ff3
TECH DEALZ <TECH DEALZ@DESKTOP-MROBLNE>
2024-09-09 09:31:01 +0000

    move to github

---
+++ src/main/java/com/mycompany/mavenproject20/Mavenproject20.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject20;
+
+/**
+ *
+ * @author TECH DEALZ
+ */
+public class Mavenproject20 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
